0165f740434c0378cf10c3211b8707e3d199c282
HenriqueChepli <131203079+HenriqueChepli@users.noreply.github.com>
2023-12-20 12:36:59 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,77 @@
+let input = document.querySelector("#display");
+let operador = "";
+let parar = false;
 
-function inserir(num){
-  document.querySelector(".tela").innerHTML += num;
-} 
-function limpar(){
-  document.querySelector(".tela").innerHTML = "";
+function limpar() {
+  input.value = "";
+  document.querySelector("#history").innerHTML = "";
+  operador = "";
 }
-function apagar(){
- let tela = document.querySelector(".tela").innerHTML;
- document.querySelector(".tela").innerHTML = tela.substring(0, tela.length -1); 
-}
-function calcular(){
-  let tela = document.querySelector(".tela").innerHTML;
-  document.querySelector('.tela').innerHTML = eval(tela) 
-  if(tela){
-      document.querySelector('.tela').innerHTML = eval(tela)
+
+function add_numero(string) {
+  if (!parar) {
+    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
+      input.value = input.value + string;
+      history.value = input.value;
+    }
+  } else {
+    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
+      input.value = "";
+      parar = false;
+      document.querySelector("#history").innerHTML = "";
+      operador = "";
+
+      input.value = input.value + string;
+    }
   }
-  else{
-      document.querySelector(".tela").innerHTML = "Erro"
+}
+
+function add_operador(op) {
+  if (input.value == "") {
+    input.value = "0";
+  }
+
+  if (operador == "") {
+    operador = op;
+    input.value = input.value + op;
+  } else {
+    total()
+    input.value = input.value + op;
+    operador = op;
+    parar=false;
+  }
+}
+
+function total() {
+  if (!parar) {
+    numeros = input.value.split(operador);
+
+    if (numeros[1] == "") {
+      input.value = input.value + '0';
+      numeros[1] = 0;
+
+    }
+
+    document.querySelector("#history").innerHTML = input.value;
+
+    switch (operador) {
+      case "+":
+        input.value = Number(numeros[0]) + Number(numeros[1]);
+        break;
+
+      case "-":
+        input.value = Number(numeros[0]) - Number(numeros[1]);
+        break;
+
+      case "x":
+        input.value = Number(numeros[0]) * Number(numeros[1]);
+        break;
+
+      case "/":
+        input.value = Number(numeros[0]) / Number(numeros[1]);
+        break;
+    }
+
+    parar = true;
   }
 }
